247a0b03934a85e99009dedd125f21d38a65657e
Ishamudgalll <47916019+Ishamudgalll@users.noreply.github.com>
2021-08-13 14:15:18 +0530

    Add files via upload

---
+++ flask_api.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug 13 13:10:42 2021
+
+@author: isham
+"""
+
+import numpy as np
+import pickle
+import math
+
+from flask import Flask, jsonify, request 
+import FundReturn
+
+app = Flask(__name__) 
+model = pickle.load(open('final_model_v3.pkl', 'rb'))
+
+@app.route('/')
+def home(): 
+	
+		return jsonify({"message" : "Hi! there"}) 
+
+
+@app.route('/home/predict', methods = ['POST']) 
+def predict():
+	
+    data = request.get_json(force=True)
+    json = list(data.values())
+    prediction = model.predict([np.array(json)])
+
+    output = prediction[0]
+    dic = {}
+    dic["key"] = output
+    return jsonify(dic)
+
+ 
+if __name__ == '__main__': 
+
+	app.run(debug = True)
